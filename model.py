2e351e580225a1c5a32b1f80853c476c9521a320
Erich Grunewald <erichgrunewald@gmail.com>
2018-11-25 22:14:34 +0100

    Remove one hidden layer (seems to not work well).

--- model.py
+++ model.py
@@ -39,8 +39,6 @@ def create_model(batch_size):
                                  dropout=hidden_dropout), merge_mode='sum'))
     model.add(Bidirectional(LSTM(output_size, activation='relu', return_sequences=True,
                                  dropout=hidden_dropout), merge_mode='sum'))
-    model.add(Bidirectional(LSTM(output_size, activation='relu', return_sequences=True,
-                                 dropout=hidden_dropout), merge_mode='sum'))
     model.compile(loss='mse', optimizer=Adam(
         lr=0.001, clipnorm=1), metrics=['mse'])
 
