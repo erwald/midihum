c4d1928dc0f4898184b28837e13e82c4a8253520
Erich Grunewald <erichgrunewald@gmail.com>
2018-12-19 01:32:37 +0100

    Add feature: time elapsed since previous note.

--- linear_regressor.py
+++ linear_regressor.py
@@ -43,11 +43,19 @@ midi_df['velocity'] = preprocessing.minmax_scale(
     midi_df.velocity.values, feature_range=(-1, 1))
 
 category_names = ['pitch_class']
-continuous_names = ['pitch', 'octave', 'nearness_to_end',
-                    'nearness_to_midpoint', 'song_duration', 'number_of_notes',
-                    'num_of_notes_adj_by_dur', 'interval_from_released',
-                    'interval_from_pressed', 'sustain',
-                    'mean_sustain', 'sustain_adj_by_mean']
+continuous_names = ['pitch',
+                    'octave',
+                    'nearness_to_end',
+                    'nearness_to_midpoint',
+                    'number_of_notes',
+                    'num_of_notes_adj_by_dur',
+                    'interval_from_released',
+                    'interval_from_pressed',
+                    'sustain',
+                    'song_duration',
+                    'time_since_last_note',
+                    'mean_sustain',
+                    'sustain_adj_by_mean']
 dep_var = 'velocity'
 
 procs = [Categorify, Normalize]
--- midi_dataframe_converter.py
+++ midi_dataframe_converter.py
@@ -94,13 +94,14 @@ def midi_file_to_data_frame(midi_file):
             assert not (any(p == pitch for p, _, _ in currently_playing_notes)
                         ), 'Pitch played again before previous one ended'
 
-            # Get interval after the last released note.
+            # Get interval after the last released note by getting that note and
+            # checking the difference between the pitch values.
             if len(result) > 0:
-                interval_from_last_released_pitch = pitch - result[-1][1]
+                interval_from_last_released_pitch = pitch - result[-1][2]
             else:
                 interval_from_last_released_pitch = 0
 
-            # Get interval after the last pressed note.
+            # Get interval after the last pressed note in a similar manner.
             if len(currently_playing_notes) > 0:
                 interval_from_last_pressed_pitch = (
                     pitch - currently_playing_notes[-1][0])
@@ -108,6 +109,7 @@ def midi_file_to_data_frame(midi_file):
                 interval_from_last_pressed_pitch = interval_from_last_released_pitch
 
             note_on_data = [velocity,
+                            time,
                             pitch,
                             pitch % 12,
                             pitch // 12,
@@ -118,8 +120,8 @@ def midi_file_to_data_frame(midi_file):
 
             currently_playing_notes.append((pitch, time, note_on_data))
         elif (msg_type == 'note_off' or (msg_type == 'note_on' and velocity == 0)):
-            assert (any(p == pitch for p, _, _ in currently_playing_notes)
-                    ), 'Encountered note off event for pitch that has not been played'
+            if any(p == pitch for p, _, _ in currently_playing_notes):
+                print('Encountered note off event for pitch that has not been played')
 
             note_on = _, note_on_time, note_on_data = next(
                 x for x in currently_playing_notes if x[0] == pitch)
@@ -130,13 +132,18 @@ def midi_file_to_data_frame(midi_file):
 
             note_off_data = [sustain_duration]
 
+            # Add new row to result and sort all rows by note time (2nd column).
             result.append(note_on_data + note_off_data)
+            result.sort(key=lambda row: row[1])
 
     df = pd.DataFrame(result)
-    df.columns = ['velocity', 'pitch', 'pitch_class', 'octave',
+    df.columns = ['velocity', 'time', 'pitch', 'pitch_class', 'octave',
                   'nearness_to_end', 'nearness_to_midpoint',
                   'interval_from_released', 'interval_from_pressed', 'sustain']
+
     df['song_duration'] = song_duration
+
+    df['time_since_last_note'] = (df.time - df.time.shift(1)).fillna(0)
 
     return df
 
