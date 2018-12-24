a5b18813a0e16ca921890b5ef9ee3a5a0452c6e5
Erich Grunewald <erichgrunewald@gmail.com>
2018-12-25 00:40:27 +0100

    Change default number of epochs from 5 to 3.

--- main_tabular.py
+++ main_tabular.py
@@ -9,7 +9,7 @@ parser.add_argument('-p', '--prepare-data', action='store_true',
                     help='converts train and validation sets to data frames and saves them')
 parser.add_argument('-t', '--train', action='store_true',
                     help='trains the model for the set number of epochs, learning rate and weight decay')
-parser.add_argument('--epochs', default=5, type=int, help='epochs')
+parser.add_argument('--epochs', default=3, type=int, help='epochs')
 parser.add_argument('--lr', default=1e-3, type=float, help='learning rate')
 parser.add_argument('--wd', default=0.2, type=float, help='weight decay')
 parser.add_argument('-humanize', help='humanizes midi file on given path')
